86fee4475c31fa5380c246a22a6b75de715c40f2
Titus <titus.kamunya@student.moringaschool.com>
2022-09-08 15:56:31 +0300

    complated migrations

--- db/migrate/20220908124131_create_poems.rb
+++ db/migrate/20220908124131_create_poems.rb
@@ -1,4 +1,10 @@
 class CreatePoems < ActiveRecord::Migration[6.1]
   def change
+    create_table :poems do |t|
+      t.string "title"
+      t.text "lines"
+      t.integer "author_id"
+      t.integer "linecount"
+  end
   end
 end
