0f8e6e886f44cb2a737ee107d15c26e6164b373c
Titus <titus.kamunya@student.moringaschool.com>
2022-09-09 11:03:45 +0300

    this commit defines my routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,23 @@ class ApplicationController < Sinatra::Base
   get "/" do
     { message: "Good luck with your project!" }.to_json
   end
+  get '/poems' do
+    poems = Poem.all
+    poems.to_json
+  end
+  post '/poems' do
+    poem= Poem.create(params)
+    poem.to_json 
+  end
 
+   post '/poems' do
+    poem= Poem.create(
+      title: params[:title], lines: params[:lines], author: params[:author], linecount: params[:linecount]
+    )
+   end
+  delete '/poems/:id' do
+    poem = Poem.find(params)[:id]
+    poem.destroy
+    poem.to_json
+  end
 end
